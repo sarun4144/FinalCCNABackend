4a49e99783949d875a300a27a8973be1fde55048
sarun4144 <sarun4144@gmail.com>
2022-11-28 18:13:23 +0700

    Sarun

--- server/Database/Config.js
+++ server/Database/Config.js
@@ -1,13 +1,13 @@
 const MongoClient = require( 'mongodb' ).MongoClient;
 const url = process.env.DATABASE
 
-var _db;
+
 
 module.exports = {
 
   connectToServer: function( callback ) {
     MongoClient.connect( url,  { useNewUrlParser: true }, function( err, client ) {
-      _db  = client.db();
+      var _db  = client.db();
       console.log('Connect Mongo DB')
       return callback( err );
     } );

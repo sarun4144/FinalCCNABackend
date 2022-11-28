20ae8380a49c9e4849da60043b272ae80b796ebb
sarun4144 <sarun4144@gmail.com>
2022-11-28 17:09:23 +0700

    Sarun

--- server/server.js
+++ server/server.js
@@ -9,11 +9,11 @@ const Mymodule = require('./Module/getCurrentTime')
 const ConectNativedatabase = require('./Database/Config')
 const cookieParser = require('cookie-parser')
 
-const Auth = require('./Routes/Auth')
-const Category = require('./Routes/Category')
-const Cloud = require('./Routes/CloudDinary')
-const Exam = require('./Routes/Exam')
-const Person = require('./Routes/Person')
+// const Auth = require('./Routes/Auth')
+// const Category = require('./Routes/Category')
+// const Cloud = require('./Routes/CloudDinary')
+// const Exam = require('./Routes/Exam')
+// const Person = require('./Routes/Person')
 //app
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -25,7 +25,7 @@ console.log(Mymodule.getCurrentTime())
 app.use(cookieParser())
 
 //DB
-// Conectdatabase()
+Conectdatabase()
 ConectNativedatabase.connectToServer(function( err, client ) {
     if (err) console.log(err);
     // start the rest of your app here
@@ -37,10 +37,10 @@ app.use(bodyParser.json({ limit: "2mb" }));
 app.use(cors())
 
 //route
-// readdirSync('./Routes').map((r) => app.use("/api", require('./Routes/' + r)));
+readdirSync('./Routes').map((r) => app.use("/api", require('./Routes/' + r)));
 
-app.use('/Auth',Auth)
-app.use('/Category',Category)
-app.use('/Cloud',Cloud)
-app.use('/Exam',Exam)
-app.use('/Person',Person)
+// app.use('/Auth',Auth)
+// app.use('/Category',Category)
+// app.use('/Cloud',Cloud)
+// app.use('/Exam',Exam)
+// app.use('/Person',Person)

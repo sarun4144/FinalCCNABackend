4e0492ea00190a1418ec384d88c120aff01220e8
sarun4144 <sarun4144@gmail.com>
2022-11-28 16:46:50 +0700

    Sarun

--- server/server.js
+++ server/server.js
@@ -9,6 +9,11 @@ const Mymodule = require('./Module/getCurrentTime')
 const ConectNativedatabase = require('./Database/Config')
 const cookieParser = require('cookie-parser')
 
+const Auth = require('./Routes/Auth')
+const Category = require('./Routes/Category')
+const Cloud = require('./Routes/CloudDinary')
+const Exam = require('./Routes/Exam')
+const Person = require('./Routes/Person')
 //app
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -32,4 +37,10 @@ app.use(bodyParser.json({ limit: "2mb" }));
 app.use(cors())
 
 //route
-readdirSync('./Routes').map((r) => app.use("/api", require('./Routes/' + r)));
+// readdirSync('./Routes').map((r) => app.use("/api", require('./Routes/' + r)));
+
+app.use('/Auth',Auth)
+app.use('/Category',Category)
+app.use('/Cloud',Cloud)
+app.use('/Exam',Exam)
+app.use('/Person',Person)

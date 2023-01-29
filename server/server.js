a23f2d8395aa1d167b625274be66ff944e33184b
sarun4144 <sarun4144@gmail.com>
2023-01-30 02:09:30 +0700

    Vercel

--- server/server.js
+++ server/server.js
@@ -3,17 +3,17 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
-const {readdirSync} = require('fs');
+// const {readdirSync} = require('fs');
 const Conectdatabase = require('./Database/Db');
 const Mymodule = require('./Module/getCurrentTime')
 const ConectNativedatabase = require('./Database/Config')
 const cookieParser = require('cookie-parser')
 
-// const Auth = require('./Routes/Auth')
-// const Category = require('./Routes/Category')
-// const Cloud = require('./Routes/CloudDinary')
-// const Exam = require('./Routes/Exam')
-// const Person = require('./Routes/Person')
+const Auth = require('./Routes/Auth')
+const Category = require('./Routes/Category')
+const Cloud = require('./Routes/CloudDinary')
+const Exam = require('./Routes/Exam')
+const Person = require('./Routes/Person')
 //app
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -42,10 +42,10 @@ app.use(bodyParser.json({ limit: "2mb" }));
 app.use(cors())
 
 //route
-readdirSync("./Routes").map((r) => app.use("/api", require('./Routes/' + r)));
+// readdirSync("./Routes").map((r) => app.use("/api", require('./Routes/' + r)));
 
-// app.use('/Auth',Auth)
-// app.use('/Category',Category)
-// app.use('/Cloud',Cloud)
-// app.use('/Exam',Exam)
-// app.use('/Person',Person)
+app.use('/Auth',Auth)
+app.use('/Category',Category)
+app.use('/Cloud',Cloud)
+app.use('/Exam',Exam)
+app.use('/Person',Person)
